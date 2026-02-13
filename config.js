f7ff700b665252239b8f782a542ebed71b5eceec
sgisasfds-beep <sgisasfds@gmail.com>
2026-02-13 13:52:10 -0500

    ..

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // URL del backend expuesto con ngrok
-const API_NGROK = "https://a200-186-180-6-146.ngrok-free.app";
+const API_NGROK = "https://aecc-186-180-6-146.ngrok-free.app";
 
 // URL del backend local (solo para desarrollo en tu PC)
 const API_LOCAL = "http://localhost:4000";
