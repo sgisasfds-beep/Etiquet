474af45721898d14d78556f4da39a26bd02408b8
sgisasfds-beep <sgisasfds@gmail.com>
2026-07-01 11:36:32 -0500

    ..

--- config.js
+++ config.js
@@ -1,23 +1,20 @@
 // URL del backend expuesto con ngrok
 const API_NGROK = "https://f29d-186-180-6-146.ngrok-free.app";
 
-// URL del backend local (solo para desarrollo en tu PC)
 const API_LOCAL = "http://localhost:4000";
 
 function getApiURL() {
     const host = window.location.hostname;
 
-    // ⭐ Si estás en Render → usar siempre ngrok
+
     if (host.includes("onrender.com")) {
         return API_NGROK;
     }
 
-    // ⭐ Si estás ejecutando el HTML directamente en tu PC
     if (host === "localhost" || host === "127.0.0.1") {
         return API_LOCAL;
     }
 
-    // ⭐ GitHub Pages, Netlify, dominio propio → usar ngrok
     return API_NGROK;
 }
 
