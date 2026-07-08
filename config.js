687626e1e3ee789322957acda4f89ed7dff55c3d
sgisasfds@gmail.com <sgisasfds@gmail.com>
2026-07-08 10:14:27 -0500

    ..

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // URL del backend expuesto con ngrok
-const API_NGROK = "https://f29d-186-180-6-146.ngrok-free.app";
+const API_NGROK = "https://5ee8-186-180-6-146.ngrok-free.app";
 
 const API_LOCAL = "http://localhost:4000";
 
