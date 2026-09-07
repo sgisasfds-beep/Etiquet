5ea35a11bff481c4f6da8c10debdd397feb812e6
sgisasfds@gmail.com <sgisasfds@gmail.com>
2026-09-07 10:56:21 -0500

    ..

--- config.js
+++ config.js
@@ -1,21 +1,14 @@
-// URL del backend expuesto con ngrok
-const API_NGROK = "https://6a9e-186-180-6-146.ngrok-free.app";
-
+const API_RENDER = "https://tu-backend-nombre.onrender.com";
 const API_LOCAL = "http://localhost:4000";
 
 function getApiURL() {
     const host = window.location.hostname;
 
-
-    if (host.includes("onrender.com")) {
-        return API_NGROK;
-    }
-
     if (host === "localhost" || host === "127.0.0.1") {
         return API_LOCAL;
     }
 
-    return API_NGROK;
+    return API_RENDER;
 }
 
 export const API_URL = getApiURL();
